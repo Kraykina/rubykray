442c11218dbc9539cf2bfcd4905b14e5f7b3982e
Olga Lomakina <olga.kraykina904@gmail.com>
2021-09-02 11:05:52 +0100

    Lesson 8

---
+++ part1/lesson8/week.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+WEEK = {
+  1 => 'понедельник',
+  2 => 'вторник',
+  3 => 'среда',
+  4 => 'четверг',
+  5 => 'пятница',
+  6 => 'суббота',
+  7 => 'воскресенье'
+}.freeze
+
+def week(number)
+  WEEK[number]  
+end
+
+puts week(rand(1..10))
