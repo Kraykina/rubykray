29e3056440d051dd6c70fca8069cfb6e7a857fad
Olga Lomakina <olga.kraykina904@gmail.com>
2021-11-15 19:26:59 +0000

    Lesson 7

---
+++ part3/lesson7/app1.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+COUNTRIES = {
+  Germany: 83_124_418,
+  France: 64_990_511,
+  Italy: 60_627_291,
+  Spain: 46_692_858,
+  Greece: 10_522_246,
+  Switzerland: 8_525_611
+}.freeze
+
+puts COUNTRIES
